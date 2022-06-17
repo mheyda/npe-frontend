80aa35f14b006416d1dd365d38272544f2680a1c
Marshall <marshallheyda@gmail.com>
2022-06-16 23:32:31 -0500

    Improved image slideshow for better mobile experience

--- frontend/src/components/slideShows/AutoSlideshow.js
+++ frontend/src/components/slideShows/AutoSlideshow.js
@@ -5,6 +5,7 @@ import './Slideshow.css';
 export default function AutoSlideshow( { images } ) {
 
     const [imgIndex, setImgIndex] = useState(0);
+    const [dotsStyling, setDotsStyling] = useState({ transform: 'translateX(0px)', transition: '0.8s' })
     const timeoutRef = useRef(null);
     const delay = 3000;
 
@@ -15,6 +16,7 @@ export default function AutoSlideshow( { images } ) {
         }
     }
 
+    // Set timer for slide change
     useEffect(() => {
         resetTimeout();
         timeoutRef.current = setTimeout(
@@ -30,18 +32,41 @@ export default function AutoSlideshow( { images } ) {
         };
     }, [imgIndex, images.length]);
 
+    // Logic for slideshow dot carousel
+    useEffect(() => {
+        if (images.length > 4) {
+            console.log("Index: " + imgIndex)
+            if (imgIndex > 1 && imgIndex < images.length - 2) {
+                console.log('greater than 2')
+                setDotsStyling({ transform: `translateX(calc(${-imgIndex  * 22}px + 44px))`, transition: '0.8s' });
+            } else if (imgIndex === 0) {
+                console.log('equals zero')
+                setDotsStyling({ transform: 'translateX(0px)', transition: '0.8s' });
+            }
+            else if (imgIndex === images.length - 1) {
+                console.log('last slide')
+                setDotsStyling({ transform: `translateX(calc(${-imgIndex * 22}px + 88px))`, transition: '0.8s'})
+            }
+        }
+    }, [imgIndex, images.length])
 
     return (
-        <div className="slideshow">
-            <div className="slideshow-slider" style={{ transform: `translate3d(${-imgIndex * 100}%, 0, 0)` }}>
+        <div className='slideshow'>
+            <div className='slideshow-btns'>
+                <i className='slideshow-left fa-solid fa-circle-arrow-left' onClick={(e) => {imgIndex === 0 ? setImgIndex(images.length - 1) : setImgIndex(imgIndex - 1); e.preventDefault()}}></i>
+                <i className="slideshow-right fa-solid fa-circle-arrow-right" onClick={(e) => {imgIndex === images.length - 1 ? setImgIndex(0) : setImgIndex(imgIndex + 1); e.preventDefault()}}></i>
+            </div>
+            <div className="slides" >
                 {images.map((image, index) => (
-                    <img key={index} className='slide' src={image.url} alt={image.altText} loading='lazy' />
+                    <img className='slide' key={index} src={image.url} alt={image.altText} style={{ transform: `translateX(${-imgIndex * 100}%)`, transition: '0.8s' }} loading='lazy' />
                 ))}
             </div>
-            <div className="slideshow-dots">
-                {images.map((_, index) => (
-                    <div key={index} className={`slideshow-dot${imgIndex === index ? " active" : ""}`} onClick={() => {setImgIndex(index);}}></div>
-                ))}
+            <div className='slideshow-dots-container'>
+                <div className="slideshow-dots" style={dotsStyling} >
+                    {images.map((_, index) => (
+                        <div key={index} className={`slideshow-dot${imgIndex === index ? " active" : ""}`} onClick={() => {setImgIndex(index);}}></div>
+                    ))}
+                </div>
             </div>
         </div>
     );
--- frontend/src/components/slideShows/ManualSlideshow.js
+++ frontend/src/components/slideShows/ManualSlideshow.js
@@ -1,21 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Slideshow.css';
 
 
 export default function ManualSlideshow( { images } ) {
 
     const [imgIndex, setImgIndex] = useState(0);
+    const [dotsStyling, setDotsStyling] = useState({ transform: 'translateX(0px)', transition: '0.8s' })
+
+    useEffect(() => {
+        if (images.length > 4) {
+            console.log("Index: " + imgIndex)
+            if (imgIndex > 1 && imgIndex < images.length - 2) {
+                console.log('greater than 2')
+                setDotsStyling({ transform: `translateX(calc(${-imgIndex  * 22}px + 44px))`, transition: '0.8s' });
+            } else if (imgIndex === 0) {
+                console.log('equals zero')
+                setDotsStyling({ transform: 'translateX(0px)', transition: '0.8s' });
+            }
+            else if (imgIndex === images.length - 1) {
+                console.log('last slide')
+                setDotsStyling({ transform: `translateX(calc(${-imgIndex * 22}px + 88px))`, transition: '0.8s'})
+            }
+        }
+    }, [imgIndex, images.length])
 
     return (
-        <div className={'slideshow'}>
+        <div className='slideshow'>
             <div className='slideshow-btns'>
                 <i className='slideshow-left fa-solid fa-circle-arrow-left' onClick={(e) => {imgIndex === 0 ? setImgIndex(images.length - 1) : setImgIndex(imgIndex - 1); e.preventDefault()}}></i>
                 <i className="slideshow-right fa-solid fa-circle-arrow-right" onClick={(e) => {imgIndex === images.length - 1 ? setImgIndex(0) : setImgIndex(imgIndex + 1); e.preventDefault()}}></i>
             </div>
-            <div className="slideshow-slider" style={{ transform: `translate3d(${-imgIndex * 100}%, 0, 0)` }}>
+            <div className="slides" >
                 {images.map((image, index) => (
-                    <img key={index} className={'slide'} src={image.url} alt={image.altText} loading='lazy' />
+                    <img className='slide' key={index} src={image.url} alt={image.altText} style={{ transform: `translateX(${-imgIndex * 100}%)`, transition: '0.8s' }} loading='lazy' />
                 ))}
+            </div>
+            <div className='slideshow-dots-container'>
+                <div className="slideshow-dots" style={dotsStyling} >
+                    {images.map((_, index) => (
+                        <div key={index} className={`slideshow-dot${imgIndex === index ? " active" : ""}`} onClick={() => {setImgIndex(index);}}></div>
+                    ))}
+                </div>
             </div>
         </div>
     );
