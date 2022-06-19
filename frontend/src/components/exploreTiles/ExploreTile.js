3458df84c563a63bba829462daa8f9902946b3b6
Marshall <marshallheyda@gmail.com>
2022-06-19 15:17:35 -0500

    Keep DOM elements to minimum until needed

--- frontend/src/components/exploreTiles/ExploreTile.js
+++ frontend/src/components/exploreTiles/ExploreTile.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import ManualSlideshow from '../slideShows/ManualSlideshow';
+import { useInView } from 'react-intersection-observer';
 
 
 export default function ExploreTile( { park } ) {
@@ -11,19 +12,32 @@ export default function ExploreTile( { park } ) {
         states = states.split(',').slice(0, 3).join(', ') + ', & More'
     }
 
-    return (
-        <Link to={`${park.fullName}/${park.parkCode}`} onClick={() => sessionStorage.setItem('currentPark', JSON.stringify(park))}>
-            <li className='explore-tile' id={park.fullName} >
-                <div className='explore-tile-img-container'>
-                    <div className='explore-tile-img'>
-                        <ManualSlideshow images={park.images} />
+    // Keep DOM elements to minimum until they are needed / in view
+    const { ref, inView } = useInView({
+        threshold: 0,
+        rootMargin: '0px 0px 200px 0px',
+        triggerOnce: true,
+    });
+
+    if (inView) {
+        return (
+            <Link to={`${park.fullName}/${park.parkCode}`} onClick={() => sessionStorage.setItem('currentPark', JSON.stringify(park))}>
+                <li className='explore-tile' id={park.fullName} >
+                    <div className='explore-tile-img-container'>
+                        <div className='explore-tile-img'>
+                            <ManualSlideshow images={park.images} />
+                        </div>
                     </div>
-                </div>
-                <div className='explore-tile-content'>
-                    <p className='explore-tile-title'>{park.name}<br></br>{park.designation}</p>
-                    <p className='explore-tile-states'>{states}</p>
-                </div>
-            </li>
-        </Link>
-    );
+                    <div className='explore-tile-content'>
+                        <p className='explore-tile-title'>{park.name}<br></br>{park.designation}</p>
+                        <p className='explore-tile-states'>{states}</p>
+                    </div>
+                </li>
+            </Link>
+        );
+    } else {
+        return (
+            <div ref={ref}></div>
+        );
+    }
 }
