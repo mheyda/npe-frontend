6e0862633498add60d52296860c7088e5c4a4369
Marshall <marshallheyda@gmail.com>
2022-06-21 23:19:58 -0500

    Finished search, sort, and filter styling

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -1,81 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectFilter, setFilter, setQuery, setSort } from '../../features/parks/parksSlice.js';
-import Select from '../select/Select.js';
+import { setQuery } from '../../features/parks/parksSlice.js';
 import SearchBar from '../searchBar/SearchBar';
-import { selectView } from '../../features/parks/parksSlice.js';
+import FilterPage from '../filterPage/FilterPage.js';
 import './NavBar.css';
 
 export default function NavBar() {
     
     const dispatch = useDispatch();
-    const filter = useSelector(selectFilter);
-    const view = useSelector(selectView);
-
-
-    const designationOptions = [
-        {
-            title: 'National Parks',
-            value: 'National Park',
-        }, 
-        {
-            title: 'National Monuments',
-            value: 'Monument'
-        }, 
-        {
-            title: 'National Preserves',
-            value: 'Preserve'
-        }, 
-        {
-            title: 'National Historical Parks',
-            value: 'Historic'
-        }
-    ];
-
-    const sortSelectOptions = [
-        {
-            title: 'Alphabetical (A-Z)',
-            value: 'Alphabetical (A-Z)'
-        }, 
-        {
-            title: 'Reverse Alphabetical (Z-A)',
-            value: 'Reverse Alphabetical (Z-A)'
-        }, 
-    ];
-
-    const updateStateCodes = (e) => {
-        if (e.target.checked) {
-            let newStateCodes = filter.stateCodes;
-            newStateCodes = [...newStateCodes, e.target.value];
-            const newFilter = {...filter, stateCodes: newStateCodes};
-            dispatch(setFilter(newFilter));
-        } else {
-            let newStateCodes = filter.stateCodes;
-            newStateCodes = newStateCodes.filter(stateCode => stateCode !== e.target.value);
-            const newFilter = {...filter, stateCodes: newStateCodes};
-            dispatch(setFilter(newFilter));
-        }
-    }
-
-    const updateDesignations = (e) => {
-        if (e.target.checked) {
-            let newDesignations = filter.designations;
-            newDesignations = [...newDesignations, e.target.value];
-            const newFilter = {...filter, designations: newDesignations};
-            dispatch(setFilter(newFilter));
-        } else {
-            let newDesignations = filter.designations;
-            newDesignations = newDesignations.filter(designation => designation !== e.target.value);
-            const newFilter = {...filter, designations: newDesignations};
-            dispatch(setFilter(newFilter));
-        }
-    }
-
-    const updateSort = (e) => {
-        const selectedIndex = e.target.options.selectedIndex;
-        const newSort = e.target.options[selectedIndex].value;
-        dispatch(setSort(newSort));
-    }
+    const [filtersOpen, setFiltersOpen] = useState(false);
 
     const handleSearch = (e) => {
         e.preventDefault();
@@ -99,38 +33,21 @@ export default function NavBar() {
 
     return (
         <nav className='navbar-container'>
-                <div className='navbar'>{/*}
-                    <div className='options-bar'>
-                        <Select name={'Sort By '} options={sortSelectOptions} onChange={updateSort} /> 
-                        <SearchBar handleSearch={handleSearch} />
-
-                        <label htmlFor="National Parks">National Parks</label>
-                        <input type="checkbox" name="National Parks" value="National Park" onChange={updateDesignations} checked={filter.designations.includes('National Park') ? true : false}></input>
-                        <label htmlFor="National Monuments">National Monuments</label>
-                        <input type="checkbox" name="National Monuments" value="Monument" onChange={updateDesignations} checked={filter.designations.includes('Monument') ? true : false}></input>
-                        <label htmlFor="National Preserves">National Preserves</label>
-                        <input type="checkbox" name="National Preservers" value="Preserve" onChange={updateDesignations} checked={filter.designations.includes('Preserve') ? true : false}></input>
-                        <label htmlFor="National Historic Parks">National Historic Parks</label>
-                        <input type="checkbox" name="National Historic Parks" value="Historic" onChange={updateDesignations} checked={filter.designations.includes('Historic') ? true : false}></input>
-
-                        <label htmlFor="MN">MN</label>
-                        <input type="checkbox" name="MN" value="MN" onChange={updateStateCodes} checked={filter.stateCodes.includes('MN') ? true : false}></input>
-                        <label htmlFor="WI">WI</label>
-                        <input type="checkbox" name="WI" value="WI" onChange={updateStateCodes} checked={filter.stateCodes.includes('WI') ? true : false}></input>
-                        
-
-                    </div>
-                    */}
-
+                <div className='navbar'>
                     <div className='options-bar'>
                         <Link className='nav-logo' to={'/'}>
                             <img src={require('../../assets/images/nps-logo.png')} alt={'National Park Service Logo'} />
                         </Link>
                         <p className='nav-title'>National<br></br>Park<br></br>Explorer</p>
-                        <button className='options-btn'><i className="fa-solid fa-filter"></i></button>
+                        <button className='options-btn' onClick={() => {
+                            setFiltersOpen(true);
+                        }}>
+                            <i className="fa-solid fa-filter"></i>
+                        </button>
                         <button className='options-btn search-toggler' onClick={toggleSearchBar}><i className="fa-solid fa-magnifying-glass"></i></button>
                     </div>
                     <SearchBar handleSearch={handleSearch} />
+                    {filtersOpen ? <FilterPage setFiltersOpen={setFiltersOpen} /> : <></>}
             </div>
         </nav>
     );
