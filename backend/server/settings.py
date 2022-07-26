a0ac78ee917a01aca4092f7054c4c8e83e560cb8
Marshall <marshallheyda@gmail.com>
2022-07-25 22:19:26 -0500

    Configured Postgres... trying to test now

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -119,10 +119,17 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'national-park-explorer',
+        'USER': env.str('DATABASE_USER'),
+        'PASSWORD': env.str('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 AUTH_USER_MODEL = 'national_park_explorer.CustomUser'
 
