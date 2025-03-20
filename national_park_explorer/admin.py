626dd6bd3a1f90a5d50f681d7d3bf58a355e1688
Marshall <marshallheyda@gmail.com>
2025-03-19 22:12:30 -0400

    Move to separate repo

--- national_park_explorer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Favorite
-
-class CustomUserAdmin(admin.ModelAdmin):
-    model = CustomUser
-
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Favorite)
